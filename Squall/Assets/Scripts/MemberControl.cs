81f145c2cf06a71f0615381946666f50da788ce0
sugahararyota <s.ryota072@gmail.com>
2020-03-29 12:04:03 +0900

    一人だけPlayerについてくるようになった

--- Squall/Assets/Scripts/MemberControl.cs
+++ Squall/Assets/Scripts/MemberControl.cs
@@ -15,15 +15,46 @@ public class MemberControl : MonoBehaviour
         isAlive,//生きてる
         isDaed,//死んでる
         isHub//拠点
-    }
-
+    }  
     private MemberStates memberStates;//メンバーの状態
     public MemberStates GetMemberState { get => memberStates; set => memberStates = value; }
+
+
+    public enum MemberCheck//メンバーがつかまっているかの状態
+    {
+        isLoitering,//徘徊してる
+        isCapture,//捕まえた
+        isDead,//死んだ
+    }
+    private MemberCheck memberCheck;
+    public MemberCheck GetMemberCheck { get => memberCheck; set => memberCheck = value; }
+
 
     // Start is called before the first frame update
     void Start()
     {
         Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        temperature = 100;
+
+        if (GetMemberCheck == MemberCheck.isLoitering)
+        {
+            WeratherCheck();
+            if (!member.pathPending && member.remainingDistance < 0.5f)
+            {
+                GotoNextPoint();
+            }
+        }
+        else
+        {
+            MoveToPlayer();
+        }
+
+        
     }
 
     public void Initialize()
@@ -32,6 +63,20 @@ public class MemberControl : MonoBehaviour
         member = gameObject.GetComponent<NavMeshAgent>();
         member.autoBraking = false;
         GotoNextPoint();
+    }
+
+    public void MoveToPlayer()
+    {
+        //目的地と自分の距離
+        Vector3 dir = GamePlayManager.instance.Player.transform.position - this.transform.position;
+        //目的地の位置
+        Vector3 pos = this.transform.position + dir * 1.5f;
+        //目的地の方向を向く
+        this.transform.rotation = Quaternion.LookRotation(dir);
+        //目的地を指定する
+        member.destination = pos;
+        //目的地からどれくらい離れて停止するか
+        member.stoppingDistance = 3.0f;
     }
 
     public void GotoNextPoint()
@@ -48,7 +93,6 @@ public class MemberControl : MonoBehaviour
         {
             member.isStopped = true;
             temperature -= 0.5f;
-            
         }
         else
         {
@@ -59,20 +103,13 @@ public class MemberControl : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("二階堂真紅");
+        if(other.gameObject.tag == "Player")
+        {
+            memberCheck = MemberCheck.isCapture;
+        }
         
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        temperature = 100;
-        WeratherCheck();
-
-        if (!member.pathPending && member.remainingDistance < 0.5f)
-        {
-            GotoNextPoint();
-        }
-    }
+    
 }
