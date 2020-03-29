9f19269d802c6c9057d62aa36da5bc63765e7e3c
sugahararyota <s.ryota072@gmail.com>
2020-03-29 10:09:05 +0900

    スコール中の仲間が止まるようになった

--- Squall/Assets/Scripts/MemberControl.cs
+++ Squall/Assets/Scripts/MemberControl.cs
@@ -5,14 +5,32 @@ using UnityEngine.AI;
 
 public class MemberControl : MonoBehaviour
 {
-    public NavMeshAgent player;
+    public NavMeshAgent member;
     public Transform[] points;
     private int destPoint = 0;
+    private float temperature = 100;
+
+    public enum MemberStates//メンバーの状態
+    {
+        isAlive,//生きてる
+        isDaed,//死んでる
+        isHub//拠点
+    }
+
+    private MemberStates memberStates;//メンバーの状態
+    public MemberStates GetMemberState { get => memberStates; set => memberStates = value; }
+
     // Start is called before the first frame update
     void Start()
     {
-        player = gameObject.GetComponent<NavMeshAgent>();
-        player.autoBraking = false;
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        memberStates = MemberStates.isAlive;
+        member = gameObject.GetComponent<NavMeshAgent>();
+        member.autoBraking = false;
         GotoNextPoint();
     }
 
@@ -20,14 +38,39 @@ public class MemberControl : MonoBehaviour
     {
         if (points.Length == 0)
             return;
-        player.destination = points[destPoint].transform.position;
+        member.destination = points[destPoint].transform.position;
         destPoint = (destPoint + 1) % points.Length;
     }
+
+    public void WeratherCheck()
+    {
+        if(GamePlayManager.instance.Weather == GamePlayManager.WeatherStates.Squall)
+        {
+            member.isStopped = true;
+            temperature -= 0.5f;
+            
+        }
+        else
+        {
+            member.isStopped = false;
+            temperature += 0.5f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("二階堂真紅");
+        
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (!player.pathPending && player.remainingDistance < 0.5f)
+        temperature = 100;
+        WeratherCheck();
+
+        if (!member.pathPending && member.remainingDistance < 0.5f)
         {
             GotoNextPoint();
         }
