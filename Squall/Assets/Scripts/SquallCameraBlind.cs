051179100583050f33f42dad646fe40a246a7959
dukecrimson <dukecrimson1013@gmail.com>
2020-03-30 17:20:06 +0900

    カメラプレハブに視界が暗いのを追加

--- Squall/Assets/Scripts/SquallCameraBlind.cs
+++ Squall/Assets/Scripts/SquallCameraBlind.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 public class SquallCameraBlind : MonoBehaviour
 {
-    public GamePlayManager gamePlayManager;
+    //GamePlayManager gamePlayManager;
     GamePlayManager.WeatherStates currentstates;//現在の天気
     GamePlayManager.WeatherStates paststate;//１フレーム前の天気
+    GamePlayManager.GamePlayStates currentstate2;//現在はマップを見てるか
+    GamePlayManager.GamePlayStates paststate2;//さっきはマップを見てたか
     //Vector3 position;//視界阻害サイズ変更用
     public int direction;//ブロックごとの方向指定数字 0=した,1=うえ,2=みぎ,3=ひだり,4=,スプライトマスク
     int count;
+    Transform transforms;
 
     // Start is called before the first frame update
     void Start()
     {
         count = 21;
-        currentstates = gamePlayManager.Weather;
+        currentstates = GamePlayManager.instance.Weather;
+        transforms = GameObject.Find("View").transform;
+        //gamePlayManager = GameObject.Find("GamePlayManager");
     }
 
     // Update is called once per frame
     void Update()
     {
         paststate = currentstates;
-        currentstates = gamePlayManager.Weather;
+        currentstates = GamePlayManager.instance.Weather;
 
         if(currentstates == GamePlayManager.WeatherStates.Squall)
         {
+            if(currentstate2 == GamePlayManager.GamePlayStates.Play && paststate2 == GamePlayManager.GamePlayStates.Map)
+            {
+                count = 21;
+                transforms.localScale -= new Vector3(0.42f,0.42f,0.42f);
+            }
+
             if (paststate == GamePlayManager.WeatherStates.Sign)
             {
                 count = 0;
@@ -66,13 +77,18 @@ public class SquallCameraBlind : MonoBehaviour
             {
                 if (count < 21)
                 {
-                    transform.localScale -= new Vector3(0.02f, 0.02f, 0.02f);
+                    transforms.localScale -= new Vector3(0.02f, 0.02f, 0.02f);
                     count++;
                 }
             }
         }
         else
         {
+            if (currentstate2 == GamePlayManager.GamePlayStates.Play && paststate2 == GamePlayManager.GamePlayStates.Map)
+            {
+                count = 21;
+                transforms.localScale += new Vector3(0.42f, 0.42f, 0.42f);
+            }
             if (paststate == GamePlayManager.WeatherStates.Squall)
             {
                 count = 0;
@@ -113,7 +129,7 @@ public class SquallCameraBlind : MonoBehaviour
             {
                 if (count < 21)
                 {
-                    transform.localScale += new Vector3(0.02f,0.02f,0.02f);
+                    transforms.localScale += new Vector3(0.02f,0.02f,0.02f);
                     count++;
                 }
             }
