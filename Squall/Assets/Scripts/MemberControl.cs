ed95da2ca9624d7f3efc039e15aa837c5fd26f8c
sugahararyota <s.ryota072@gmail.com>
2020-04-06 16:36:04 +0900

    仲間の頭の上にHpバーを追加

--- Squall/Assets/Scripts/MemberControl.cs
+++ Squall/Assets/Scripts/MemberControl.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class MemberControl : MonoBehaviour
 {
     public NavMeshAgent member;
     public Transform[] points;
     private int destPoint = 0;
-    private float temperature = 100;
+    private float memberHp = 100;
+    const float MIN = 0;
+    const float MAX = 100;
     //プレイヤーを追うための
     private Vector3 def;
     private GameObject player;
@@ -20,6 +23,7 @@ public class MemberControl : MonoBehaviour
     private int memberNumber;
     private Transform memberLingt;
     public Vector3 lightScale;
+    public Slider slider;
 
 
     public enum MemberStates//メンバーの状態
@@ -63,28 +67,39 @@ public class MemberControl : MonoBehaviour
         script = player.GetComponent<MemberList>();
         member.speed += navSpeed;
 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(memberNumber);
+        //Debug.Log(memberHp);
+        slider.value = memberHp;
+
+     
         //Memberの処理分岐
         if (GetMemberCheck == MemberCheck.isLoitering)//徘徊しているときの処理書くところ
         {
+            if (memberHp <= 0)
+                memberCheck = MemberCheck.isDead;
             WeratherCheck();
-            if (!member.pathPending && member.remainingDistance < 0.5f)//ぐるぐる回るやつ(徘徊)
+            SquallCheck();
+            if (!member.pathPending && member.remainingDistance < 0.5f)
             {
-                GotoNextPoint();
+                GotoNextPoint();//ぐるぐる回るやつ(徘徊)
             }
         }
         else if(GetMemberCheck == MemberCheck.isCapture)//プレイヤーに捕まったらの処理書くところ
         {
+            if (memberHp <= 0)
+                memberCheck = MemberCheck.isDead;
+
+            WeratherCheck();
             PlayerFollows();
             MemberHubCheck();
 
         }
-        else if (GetMemberCheck == MemberCheck.isHub)//拠点いるとき
+        else if (GetMemberCheck == MemberCheck.isHub)//拠点いるときの処理書くところ
         {
             //今のところ何もしない
         }
@@ -96,15 +111,6 @@ public class MemberControl : MonoBehaviour
 
     public void PlayerFollows()//仲間がドラクエの隊列みたいにPlayerを追いかける
     {
-        //for (int i = 1; i <= script.memberList.Count; i++)
-        //{
-        //    if (memberNumber == i)
-        //    {
-        //        member.destination = script.memberList[i - 1].transform.position;
-        //        member.stoppingDistance = navStop;//Playerが止まってからどれくらいで止まるか、
-        //    }
-        //}
-
         for (int i = 1; i < script.memberList.Count; i++)//捕まえたメンバーの数だけ回す
         {
             if (script.memberList[i] == this.gameObject)
@@ -113,31 +119,10 @@ public class MemberControl : MonoBehaviour
                 member.stoppingDistance = navStop;
             }
         }
-
         //スプライトの回転をなくす
         Vector3 parent = this.transform.parent.transform.localRotation.eulerAngles;
         this.transform.localRotation = Quaternion.Euler(def - parent);
     }
-
-
-
-    //public void MoveToPlayer()//今のところプレイヤーを追いかける
-    //{
-    //    //目的地と自分の距離
-    //    Vector3 dir = GamePlayManager.instance.Player.transform.position - this.transform.position;
-    //    //目的地の位置
-    //    Vector3 pos = this.transform.position + dir * 1.5f;
-    //    //目的地の方向を向く
-    //    this.transform.rotation = Quaternion.LookRotation(dir);
-    //    //目的地を指定する
-    //    member.destination = pos;
-    //    //目的地からどれくらい離れて停止するか
-    //    member.stoppingDistance = 2.0f;
-
-
-       
-
-    //}
 
     public void MemberToPlayer()//仲間がPlayerにつかまってから川を避けなくする
     {
@@ -152,19 +137,37 @@ public class MemberControl : MonoBehaviour
         //ぐるぐる回るやつ
         member.destination = points[destPoint].transform.position;
         destPoint = (destPoint + 1) % points.Length;
+
+        //スプライトの回転をなくす
+        
     }
 
     public void WeratherCheck()//天気を確認()
     {
-        if(GamePlayManager.instance.Weather == GamePlayManager.WeatherStates.Squall)
+        if (GamePlayManager.instance.Weather == GamePlayManager.WeatherStates.Squall)
         {
-            member.isStopped = true;
-            temperature -= 0.5f;
+            memberHp -= Time.deltaTime * 5;
+            memberHp = System.Math.Max(memberHp, MIN);//最小値を超えたら戻す
         }
-        else
+        else 
         {
-            member.isStopped = false;
-            temperature += 0.5f;
+            memberHp += Time.deltaTime * 2;
+            memberHp = System.Math.Min(memberHp, MAX);//最大値を超えたら戻す
+        }           
+    }
+
+    public void SquallCheck()
+    {
+        if (GetMemberCheck == MemberCheck.isLoitering)
+        {
+            if (GamePlayManager.instance.Weather == GamePlayManager.WeatherStates.Squall)
+            {
+                member.isStopped = true;
+            }
+            else
+            {
+                member.isStopped = false;
+            }
         }
     }
 
@@ -189,11 +192,7 @@ public class MemberControl : MonoBehaviour
             MemberToPlayer();
             script.memberList.Add(this.gameObject);
             memberLingt.transform.localScale = new Vector3(lightScale.x,lightScale.y,lightScale.z);
-           // memberNumber = script.memberList.Count - 1;
-        }
-        
-    }
-
-
-    
+            memberHp = 100;//HPを回復
+        }      
+    }    
 }
