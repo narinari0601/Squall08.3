3b83329ba7f7440c025c9b98ba05c73c2570b434
midnoy1 <machida.1331@gmail.com>
2020-05-12 17:09:02 +0900

    プレイヤー向き修正

--- Squall/Assets/Scripts/Playercontrol.cs
+++ Squall/Assets/Scripts/Playercontrol.cs
@@ -104,31 +104,67 @@ public class Playercontrol : MonoBehaviour
                 return;
 
             wind = new Vector3(0, 0, 0);
+
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 _direc = Direc.UP;
                 velocity += new Vector3(0, 0, 0.1f);
-                anim.SetInteger("Direction", 0);
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    anim.SetInteger("Direction", 0);
+                }
             }
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 velocity += new Vector3(0, 0, -0.1f);
                 _direc = Direc.DOWN;
-                anim.SetInteger("Direction", 1);
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    anim.SetInteger("Direction", 1);
+                }
             }
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 velocity += new Vector3(-0.1f, 0, 0);
                 _direc = Direc.LEFT;
-                anim.SetInteger("Direction", 2);
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    anim.SetInteger("Direction", 2);
+                }
             }
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 velocity += new Vector3(0.1f, 0, 0);
                 _direc = Direc.RIGHT;
-
-                anim.SetInteger("Direction", 3);
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    anim.SetInteger("Direction", 3);
+                }
             }
+
+            if (Mathf.Abs(velocity.x) > Mathf.Abs(velocity.z))
+            {
+                if (velocity.x < 0)
+                {
+                    anim.SetInteger("Direction", 2);
+                }
+                else if (velocity.x > 0)
+                {
+                    anim.SetInteger("Direction", 3);
+                }
+            }
+            if (Mathf.Abs(velocity.x) < Mathf.Abs(velocity.z))
+            {
+                if (velocity.z< 0)
+                {
+                    anim.SetInteger("Direction", 1);
+                }
+                else if (velocity.z > 0)
+                {
+                    anim.SetInteger("Direction", 0);
+                }
+            }
+            
             if (velocity.x * velocity.x + velocity.z * velocity.z > 0.01)
             {
                 if (velocity.x < 0)
@@ -255,8 +291,7 @@ public class Playercontrol : MonoBehaviour
         {
             transform.position = transform.position + damagevelocity;
             damagevelocity -= savevelocity / 5;
-            Debug.Log(damagevelocity);
-            
+        
         }
        // transform.position *= Time.deltaTime;
     }
