6ff7138a15cd802b12cc49847d7c758711034dcc
Narinari0601 <kouta19960601.kizuna-reo.t-mk@ezweb.ne.jp>
2020-06-12 10:39:43 +0900

    ステージ3修正

--- Squall/Assets/Scripts/GamePlayManager.cs
+++ Squall/Assets/Scripts/GamePlayManager.cs
@@ -18,10 +18,10 @@ public class GamePlayManager : MonoBehaviour
 
     public enum SquallDirections
     {
-        Up=0,
-        Down=1,
-        Left=2,
-        Right=3,
+        Up = 0,
+        Down = 1,
+        Left = 2,
+        Right = 3,
     }
 
     public enum GamePlayStates
@@ -37,8 +37,6 @@ public class GamePlayManager : MonoBehaviour
     //ゲームオブジェクト関連
     private GameObject player;
 
-    //private GameObject[] members;
-
     [SerializeField, Header("メインカメラ")]
     private GameObject mainCamera = null;
 
@@ -46,14 +44,9 @@ public class GamePlayManager : MonoBehaviour
 
     private SquallCameraBlind cameraBlind;
 
-    //private GameObject currentCamera;
-
-    //private List<Camera> cameraList;
-
     //天気関連
     private WeatherStates weather;
 
-    //[SerializeField,Header("天気が1周する時間")]
     private float weatherRotateTime;
 
     [SerializeField, Header("通常時の比率")]
@@ -67,27 +60,16 @@ public class GamePlayManager : MonoBehaviour
 
     private float toatalWeatherRatio;  //天候比の合計
 
-    //private float currentWeatherTimer;
 
     //風関連
     private SquallDirections squallDirection;
 
     private float windPower;
 
-    //[SerializeField, Header("風の方向を順番に")]
-    //private SquallDirections[] squallDirArray;
-
-    //private List<SquallDirections> squallDirList;
-
-    //private int squallCount;  //何回目のスコールか
-
-    //private bool isInsert;
-
-    //private SquallDirections interruptDir;  //割り込みスコールの方向
 
 
     //ステージ関連
-    [SerializeField,Header("ステージ")]
+    [SerializeField, Header("ステージ")]
     private GameObject[] stagePrefabs = new GameObject[0];
 
     private List<GameObject> stageList;
@@ -98,7 +80,7 @@ public class GamePlayManager : MonoBehaviour
 
     [SerializeField, Header("bake2つ")]
     private GameObject[] navBakes = new GameObject[2];
-    
+
 
 
     //ゲームの状態関連
@@ -143,7 +125,7 @@ public class GamePlayManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
-        
+
         Initialize();
     }
 
@@ -157,8 +139,6 @@ public class GamePlayManager : MonoBehaviour
 
         audioSource = GetComponent<AudioSource>();
 
-        //cameraList = new List<Camera>();
-
         foreach (var nav in navBakes)
         {
             nav.GetComponent<NavBakeScript>().Initialize();
@@ -169,8 +149,6 @@ public class GamePlayManager : MonoBehaviour
 
         isBGM = false;
 
-        //uiManager = uIObj.GetComponent<UIManager>();
-        //uiManager.Initialize();
     }
 
     public void StageInitialize()
@@ -182,7 +160,6 @@ public class GamePlayManager : MonoBehaviour
             currentStage = null;
         }
 
-        //var stage = Instantiate(stagePrefabs[stageNum]);
         var stage = Instantiate(StageData.StageList[stageNum]);
 
         foreach (var nav in navBakes)
@@ -197,65 +174,11 @@ public class GamePlayManager : MonoBehaviour
 
         gameState = GamePlayStates.Map;
 
-        //currentWeatherTimer = 0;
-
-        //squallCount = 0;
-
         weatherRotateTime = currentStage.WeatherRotateTime;
 
         windPower = currentStage.WindPower;
 
-        #region 場所移行したけどスコール関係の初期化処理残しときます
-        //squallDirArray = currentStage.SquallDirArray;
-
-        //squallDirList = new List<SquallDirections>();
-
-        //for (int i = 0; i < squallDirArray.Length; i++)
-        //{
-        //    squallDirList.Add(squallDirArray[i]);
-        //}
-
-        //squallDirection = squallDirArray[0];
-
-        //squallDirection = squallDirList[0];
-
-
-        //cameraController = mainCamera.GetComponent<CameraController>();
-        //cameraController.Initialize();
-
-        //cameraList.Clear();
-
-        //cameraList.Add(cameraController.Camera);
-        //var mapCamera = currentStage.MapCamera;
-        //cameraList.Add(mapCamera);
-        //currentCamera = cameraList[0];
-        //cameraList[1].SetActive(false);
-
-
-        //cameraList.Add(cameraController.CameraScript);
-        //var mapCamera = currentStage.MapCamera.GetComponentInChildren<Camera>();
-        //cameraList.Add(mapCamera);
-        //cameraList[0].depth = 2;
-        //cameraList[1].depth = 3;
-
-        //uiManager.Initialize();
-
-        //var currentDir = (int)squallDirArray[(squallCount + 0) % squallDirArray.Length];
-        //var secondDir = (int)squallDirArray[(squallCount + 1) % squallDirArray.Length];
-        //var thirdDir = (int)squallDirArray[(squallCount + 2) % squallDirArray.Length];
-
-        //var currentDir = (int)squallDirList[(squallCount + 0) % squallDirList.Count];
-        //var secondDir = (int)squallDirList[(squallCount + 1) % squallDirList.Count];
-        //var thirdDir = (int)squallDirList[(squallCount + 2) % squallDirList.Count];
-
-        //uiManager.WindDirectUI.ChangeDirection(currentDir, secondDir, thirdDir);
-        #endregion
-
         cameraBlind.Initialize();
-
-
-        //isInsert = false;
-        //interruptDir = SquallDirections.Up;
     }
 
     private void Update()
@@ -273,7 +196,6 @@ public class GamePlayManager : MonoBehaviour
             StageEndCheack();
             currentStage.Ripple();
             currentStage.ChangeWeather();
-            //ChangeWeather();
             PauseStart();
             uiManager.UpdatePlayUI();
         }
@@ -288,7 +210,6 @@ public class GamePlayManager : MonoBehaviour
         {
             StageEndCheack();
             currentStage.ChangeWeather();
-            //ChangeWeather();
             uiManager.PauseUI.PauseUpdate();
             uiManager.UpdatePlayUI();
         }
@@ -303,137 +224,42 @@ public class GamePlayManager : MonoBehaviour
             GameOver();
         }
 
-        //if (Input.GetKeyDown(KeyCode.Alpha1))
-        //{
-        //    NextStage();
-        //}
 
         //if (Input.GetKeyDown(KeyCode.R))
         //{
         //    BGMManager.instance.ChangeBGM(0, 0.04f);
         //    StageInitialize();
         //}
-        
-        
+
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             GameEnd();
         }
 
-        //if (Input.GetKeyDown(KeyCode.I))
-        //{
-        //    if (!isInsert)
-        //    {
-        //        isInsert = true;
-        //        squallDirList.Insert(squallCount, SquallDirections.Up);
-        //        interruptDir = SquallDirections.Up;
-        //        squallDirection = squallDirList[squallCount];
-        //    }
-        //}
 
     }
 
-    //private void ChangeWeather()
-    //{
-    //    currentWeatherTimer += Time.deltaTime;
 
-    //    if (currentWeatherTimer >= weatherRotateTime)
-    //    {
-    //        currentWeatherTimer = 0;
-
-    //        if (isInsert)
-    //        {
-    //            if (squallDirection == interruptDir)
-    //            {
-    //                isInsert = false;
-    //                squallDirList.RemoveAt(squallCount);
-    //            }
-    //        }
-
-    //        squallCount++;
-
-    //        //if (squallCount == squallDirArray.Length)
-    //        //{
-    //        //    squallCount = 0;
-    //        //}
-
-    //        if (squallCount == squallDirList.Count)
-    //        {
-    //            squallCount = 0;
-    //        }
-
-    //        //squallDirection = squallDirArray[squallCount];
-
-    //        squallDirection = squallDirList[squallCount];
-    //    }
-
-    //    //晴れ
-    //    if (currentWeatherTimer < sunRatio / toatalWeatherRatio * weatherRotateTime)
-    //    {
-    //        weather = WeatherStates.Sun;
-    //        //Debug.Log("晴れ");
-    //    }
-
-    //    //スコール
-    //    else if (currentWeatherTimer >= (toatalWeatherRatio - squallRatio) / toatalWeatherRatio * weatherRotateTime)
-    //    {
-    //        weather = WeatherStates.Squall;
-    //        uiManager.WindDirectUI.SetActive(false);
-    //        //Debug.Log("スコール");
-    //    }
-
-    //    //予兆
-    //    else
-    //    {
-    //        weather = WeatherStates.Sign;
-
-    //        //var currentDir= (int)squallDirArray[(squallCount + 0) % squallDirArray.Length];
-    //        //var secondDir = (int)squallDirArray[(squallCount + 1) % squallDirArray.Length];
-    //        //var thirdDir= (int)squallDirArray[(squallCount + 2) % squallDirArray.Length];
-
-    //        var currentDir = (int)squallDirList[(squallCount + 0) % squallDirList.Count];
-    //        var secondDir = (int)squallDirList[(squallCount + 1) % squallDirList.Count];
-    //        var thirdDir = (int)squallDirList[(squallCount + 2) % squallDirList.Count];
-    //        uiManager.WindDirectUI.ChangeDirection(currentDir, secondDir, thirdDir);
-
-    //        if (gameState == GamePlayStates.Play)
-    //        {
-    //            uiManager.WindDirectUI.SetActive(true);
-    //        }
-
-    //        //currentDirect.text = directStrings[(int)squallDirArray[squallCount]];
-    //        //secondDirect.text = directStrings[(int)secondDir];
-    //        //thirdDirect.text = directStrings[(int)thirdDir];
-    //        //Debug.Log("予兆");
-    //    }
-
-
-    //}
 
     public void NextStage()
     {
-        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        currentStage.gameObject.SetActive(false);
+        Destroy(currentStage.gameObject);
+
+        stageNum++;
+
+
+        if (stageNum > StageData.StageList.Count - 1)
         {
-            //var stage = Instantiate(stagePrefabs[stageNum]);
-            //stage.GetComponent<Stage>().Initialize();
-            //stageList[stageNum] = stage;
-
-            currentStage.gameObject.SetActive(false);
-            Destroy(currentStage.gameObject);
-
-            stageNum++;
-            
-
-            if (stageNum > StageData.StageList.Count-1)
-            {
-                stageNum = 0;
-            }
-
-
-            BGMManager.instance.ChangeBGM(0, 0.04f);
-
-            StageInitialize();
+            stageNum = 0;
         }
+
+
+        BGMManager.instance.ChangeBGM(0, 0.04f);
+
+        StageInitialize();
+
     }
 
     public void StageEndCheack()
@@ -447,38 +273,11 @@ public class GamePlayManager : MonoBehaviour
 
         if (state == GamePlayStates.Play)
         {
-            //if (currentCamera == cameraList[0])
-            //    return;
-
-            //currentCamera = cameraList[0];
-            //cameraList[1].SetActive(false);
-            //cameraList[0].SetActive(true);
-
-            //if (cameraList[0].depth < cameraList[1].depth)
-            //{
-            //    cameraList[0].depth = 3;
-            //    cameraList[1].depth = 2;
-            //}
-
             cameraController.MapToPlay();
-
         }
 
         else if (state == GamePlayStates.Map)
         {
-            //if (currentCamera == cameraList[1])
-            //    return;
-
-            //currentCamera = cameraList[1];
-            //cameraList[0].SetActive(false);
-            //cameraList[1].SetActive(true);
-
-            //if (cameraList[0].depth > cameraList[1].depth)
-            //{
-            //    cameraList[0].depth = 2;
-            //    cameraList[1].depth = 3;
-            //}
-
             cameraController.PlayToMap();
         }
     }
@@ -514,13 +313,7 @@ public class GamePlayManager : MonoBehaviour
         }
     }
 
-    public void RetryScene()
-    {
-        //if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-    }
+    
 
     public void GameClear()
     {
@@ -530,9 +323,6 @@ public class GamePlayManager : MonoBehaviour
         var gameClearUI = uiManager.GameClearUI;
         gameClearUI.SetActive(true);
         gameClearUI.ScoreCalculate();
-        //gameClearUI.RankJudgment();
-        //gameClearUI.CursolMove();
-        //gameClearUI.NextScene();
     }
 
     public void GameOver()
@@ -545,20 +335,6 @@ public class GamePlayManager : MonoBehaviour
         StageData.StageNum = stageNum;
         SceneManager.LoadScene("StageSelectScene");
     }
-
-    //public void ChangeBGM(int num, float volume)
-    //{
-    //    audioSource.Stop();
-    //    audioSource.clip = audioClips[num];
-    //    audioSource.volume = volume;
-    //    audioSource.Play();
-    //}
-
-    //public void StopBGM()
-    //{
-    //    audioSource.Stop();
-    //}
-
 
     public void GameEnd()
     {
